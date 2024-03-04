a2421e12d8a775ab27baf8f5fb60dcd56023393f
xiayeqing <xia00044@algonquinlive.com>
2024-03-03 20:08:59 -0500

    update docker.sh: read cfg form database.properties, init run schema.sql

--- app/data/docker.sh
+++ app/data/docker.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+set -x
+# Read properties from database.properties
+while IFS='=' read -r key value
+do
+    key=$(echo $key | tr '.' '_')
+    eval "${key}='${value}'"
+done < database.properties
+
 # Install MySQL
 
 docker stop mysql
@@ -5,13 +14,15 @@ docker stop mysql
 docker rm mysql
 
 docker create --name mysql \
-    -e MYSQL_DATABASE={dbname} \
-    -e MYSQL_USER={username} \
-    -e MYSQL_PASSWORD={password} \
-    -p 3306:3306 \
+    -e MYSQL_DATABASE=${name} \
+    -e MYSQL_USER=${user} \
+    -e MYSQL_PASSWORD=${pass} \
+    -e MYSQL_RANDOM_ROOT_PASSWORD=true \
+    -v $(pwd)/schema.sql:/docker-entrypoint-initdb.d/schema.sql \
+    -p ${port}:3306 \
     mysql:8.0.36
 
-docker network connect app mysql
+#docker network connect app mysql
 
 docker update --restart=always mysql
 
