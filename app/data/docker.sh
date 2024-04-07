afceb850070452b4e8bbade4f172dcc0ac7ad737
Jeremy S <jeresiv@gmail.com>
2024-04-07 12:16:53 -0400

    Updated structure and scripts

--- app/data/docker.sh
+++ app/data/docker.sh
@@ -1,14 +1,22 @@
-# Install MySQL
+#!/bin/bash
+
+# Read properties from database.properties
+while IFS='=' read -r key value
+do
+    key=$(echo $key | tr '.' '_')
+    eval "${key}='${value}'"
+done < database.properties
 
 docker stop mysql
-
 docker rm mysql
 
 docker create --name mysql \
-    -e MYSQL_DATABASE={dbname} \
-    -e MYSQL_USER={username} \
-    -e MYSQL_PASSWORD={password} \
-    -p 3306:3306 \
+    -e MYSQL_DATABASE=${name} \
+    -e MYSQL_USER=${user} \
+    -e MYSQL_PASSWORD=${pass} \
+    -e MYSQL_ROOT_PASSWORD=${pass} \
+    -v ./schema.sql:/docker-entrypoint-initdb.d/schema.sql \
+    -p ${port}:3306 \
     mysql:8.0.36
 
 docker network connect app mysql
